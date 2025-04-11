4946bd6fc8034c0f045842c90adfd218cfeed11a
m4xthunder <maxim452736@bk.ru>
2025-04-11 13:10:45 +0300

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,20 @@
+<?php header("HTTP/1.0 404 Not Found"); ?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5, minimum-scale=1">
+	<link rel="stylesheet" type="text/css" href="404.css">
+	<title>Ошибка 404</title>
+</head>
+<body>
+    <div class="backgound">
+    	<h1 class="h1">404</h1>
+    	<p class="text">Похоже вы ошиблись, перейдите на главную страницу</p>
+    	<p class="error"><em>ОШИБКА</em></p>
+    	<div class="flex">
+    	     <a class="index" href="https://vavadaa96.online">На главную</a>
+    	</div>
+    </div>
+</body>
+</html>
